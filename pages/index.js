4a449855611bf5e5c532e4fd6cbcf73df9595b0b
Ruaidhri Lumsden <ruaidhri@lumsden.digital>
2022-05-08 21:51:18 +0100

    error fix attempt 3: replaced " with ' in index page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Link from 'next/link'
-import Card from '../components/Card'
-import image1 from '../public/img/Page127.jpg'
-import image2 from '../public/img/ConcordeLanding.png'
-import ContentBlock from '../components/ContentBlock'
-import { createClient } from 'contentful'
+import React from "react"
+import Link from "next/link"
+import Card from "../components/Card"
+import image1 from "../public/img/Page127.jpg"
+import image2 from "../public/img/ConcordeLanding.png"
+import ContentBlock from "../components/ContentBlock"
+import { createClient } from "contentful"
 
 export async function getStaticProps() {
     
@@ -13,8 +13,8 @@ export async function getStaticProps() {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
     })
     
-    const res = (await client.getEntries({ content_type: 'textSection' })).items
-    const books = res.filter(item => item.fields.type === 'book')
+    const res = (await client.getEntries({ content_type: "textSection" })).items
+    const books = res.filter(item => item.fields.type === "book")
 
     return {
         props: {
@@ -30,11 +30,11 @@ export default function home(props) {
   return (
       <div>
           <ContentBlock>
-            <p className='mb-3'>Welcome to Waverley Aerospace Publications. Below are the books currently available for purchase. <strong>Touchdown, Safely!</strong> is available immediately, whereas <strong>Blind Landing, A History</strong> is available for pre-order and will be posted week commencing 23rd May.</p>
-            <p className='mb-3'>Both books can be bought together for the <strong>reduced price of £40</strong>. P&P is £5 per book.</p>
-            <p className='mb-8'>This website is under development, and so we can not currently take payment here. However, if you would like to email the author with a purchase order, payment can be made directly using PayPal. Please <strong className=' underline'><Link href='/contact'>contact the author</Link></strong> for details.</p>
+            <p className="mb-3">Welcome to Waverley Aerospace Publications. Below are the books currently available for purchase. <strong>Touchdown, Safely!</strong> is available immediately, whereas <strong>Blind Landing, A History</strong> is available for pre-order and will be posted week commencing 23rd May.</p>
+            <p className="mb-3">Both books can be bought together for the <strong>reduced price of £40</strong>. P&P is £5 per book.</p>
+            <p className="mb-8">This website is under development, and so we can not currently take payment here. However, if you would like to email the author with a purchase order, payment can be made directly using PayPal. Please <strong className="underline"><Link href="/contact">contact the author</Link></strong> for details.</p>
           </ContentBlock>
-          <div className='grid md:grid-cols-2'>
+          <div className="grid md:grid-cols-2">
             {props.books.map((book, index) => (
                 <Card 
                   key={book.fields.title}
