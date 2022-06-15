529c0cad12537318898127efedafa7b0a8d69c4c
Ruaidhri Lumsden <ruaidhri@lumsden.digital>
2022-06-15 14:33:00 +0100

    created md files and logged in getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,46 @@ import Card from "../components/Card"
 import image1 from "../public/img/Page127.jpg"
 import image2 from "../public/img/ConcordeLanding.png"
 import ContentBlock from "../components/ContentBlock"
-import { createClient } from "contentful"
+import fs from 'fs'
+import path from 'path'
 
 export async function getStaticProps() {
     
-    const client = createClient({
-        space: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+    // const res = (await client.getEntries({ content_type: "textSection" })).items
+    // const books = res.filter(item => item.fields.type === "book")
+
+    // return {
+    //     props: {
+    //         books
+    //     }
+    // }
+
+    const files = fs.readdirSync(path.join('posts'))
+
+    const posts = files.map((filename) => {
+
+      const slug = filename.replace('.md', '')
+      
+      const markdownWithMeta = fs.readFileSync(path.join('posts', filename),'utf-8')
+      console.log(markdownWithMeta)
+      
+      return {
+        slug
+      }
+      
     })
-    
-    const res = (await client.getEntries({ content_type: "textSection" })).items
-    const books = res.filter(item => item.fields.type === "book")
+
 
     return {
-        props: {
-            books
-        }
+      props: {
+        posts: 'posts'
+      }
     }
 }
 
 export default function home(props) {
+
+  console.log(props)
   
   const images = [image1, image2]
 
@@ -41,7 +61,7 @@ export default function home(props) {
             <p className="mb-8">This website is under development, and so we can not currently take payment here. However, if you would like to email the author with a purchase order, payment can be made directly using PayPal. Please <strong className="underline"><Link href="/contact">contact the author</Link></strong> for details.</p>
           </ContentBlock>
           <div className="grid md:grid-cols-2">
-            {props.books.map((book, index) => (
+            {/* {props.books.map((book, index) => (
                 <Card 
                   key={book.fields.title}
                   image={images[index]} 
@@ -49,7 +69,7 @@ export default function home(props) {
                   description={book.fields.content.content[0].content[0].value} 
                   price={book.fields.subtitle}              
                 />
-            ))}
+            ))} */}
           </div>
       </div>
   )
